356cf89f0cf6c45682cde2fdf33578ff9ded9e83
dcahun <dcahun@3code.us>
2025-03-29 12:18:04 -0600

    Mi first Commit

---
+++ NutriBebApp/app/src/main/java/com/example/nutribebapp/uiView/LoginView.kt
@@ -0,0 +1,11 @@
+package com.example.nutribebapp.uiView
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class  LoginView : Screen{
+    @Composable
+    override fun Content() {
+
+    }
+}
